12bf469f1766d66fbdb9f570858767349c102a9f
CodeJJ422 <n23ao102@gmail.com>
2025-06-30 22:02:56 +0900

    コントローラーで情報を取得

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,51 @@
+require 'httparty'
+
 class MatchesController < ApplicationController
+  before_action :set_key, only: :create
 
   def index
   end
   
+  def create
+    get_puuid
+    get_rank
+  end
+
+  private
+  def set_key
+    @api_key = ENV['RIOT_API_KEY']
+  end
+
+  def get_puuid
+    encoded_player_name = URI.encode_www_form_component(params[:player_name])
+    encoded_player_tag = URI.encode_www_form_component(params[:player_tag])
+    account_url = "https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/#{encoded_player_name}/#{encoded_player_tag}"
+    response = HTTParty.get(account_url, headers: { "X-Riot-Token" => @api_key })
+
+    if response.code == 200
+      data = JSON.parse(response.body)
+      @puuid = data["puuid"]
+    else
+      puts "エラー: #{response.code}"
+    end
+
+  end
+
+  def get_rank
+    rank_url = "https://jp1.api.riotgames.com/lol/league/v4/entries/by-puuid/#{@puuid}"
+    rank_response = HTTParty.get(rank_url, headers: { "X-Riot-Token" => @api_key })
+
+    if rank_response.code == 200
+      leagues = JSON.parse(rank_response.body)
+      solo_rank = leagues.find { |league| league["queueType"] == "RANKED_SOLO_5x5" }
+      if solo_rank
+        puts "#{solo_rank['tier']}#{solo_rank['rank']}"
+      else
+        puts "RANKED_SOLO_5x5 のデータが見つかりません。"
+      end
+    else
+      puts "ランク情報取得失敗: #{rank_response.body}"
+    end
+  end
+
 end
--- app/javascript/match_index.js
+++ app/javascript/match_index.js
@@ -4,6 +4,18 @@ const getRank = () => {
     e.preventDefault();
     const playerName = document.getElementById("players_0_summoner_name").value;
     const playerTag = document.getElementById("players_0_tag").value;
+
+    const formData = new FormData();
+    formData.append("player_name", playerName);
+    formData.append("player_tag", playerTag);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/matches", true);
+    XHR.responseType = "json";
+
+    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute("content");
+    XHR.setRequestHeader("X-CSRF-Token", csrfToken);
+    
+    XHR.send(formData);
   });
 };
 
