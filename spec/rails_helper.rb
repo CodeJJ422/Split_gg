db9ea93259f148e6c6649c8127f5d3a68614360e
CodeJJ422 <n23ao102@gmail.com>
2025-09-09 22:39:40 +0900

    webmockを導入

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  require 'webmock/rspec'
+  # Selenium / Capybara の内部通信は許可
+  WebMock.disable_net_connect!(allow_localhost: true)
 end
--- spec/system/rank_spec.rb
+++ spec/system/rank_spec.rb
@@ -5,23 +5,10 @@ RSpec.describe "ランク取得", type: :system do
     @player = FactoryBot.build(:player)
   end
 
-  context 'ユーザー新規登録ができるとき' do 
-    it '正しい情報を入力すればランク選択欄からランクが自動で選択される' do
-    # トップページに移動する
-      visit root_path
-    # ユーザー名を入力する
-      fill_in 'players_0_summoner_name', with: @player.name
-    # タグを入力する
-      fill_in 'players_0_tag', with: @player.tag
-    # ランク取得ボタンを押すとランクのvalueが""以外になっている
-      find("#get-rank").click
-      sleep 1
-      
-    end
+  context 'ランク取得ができるとき' do 
+    
   end
-  context 'ユーザー新規登録ができないとき' do
-    it '誤った情報ではランク取得はされず、エラーメッセージが出る' do
-      
-    end
+  context 'ランク取得ができないとき' do
+    
   end
 end
